ba7dd1728ccf70e51ca3d31e15509fdc2ca52d79
saraswat171 <saraswatchetan2020@gmail.com>
2024-12-02 14:30:35 +0530

    feature:- add export  to constant file objects

--- src/features/message/send-message/send-message.service.ts
+++ src/features/message/send-message/send-message.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { ChatNotFound } from 'src/domain/chat/exception/exception';
 import { PusherService } from 'src/features/pusher/pusher.servcie';
+import { channels ,events , pusherAction } from 'src/infrastructure/common/constants';
 import { ChatRepository } from 'src/infrastructure/repositories/chat/chat.repository';
 import { MessageRepository } from 'src/infrastructure/repositories/message/message.repository';
 import { UserRepository } from 'src/infrastructure/repositories/user/user.repository';
@@ -11,23 +12,28 @@ export class SendMessageService {
     private readonly messageRepository: MessageRepository,
     private readonly repository: ChatRepository,
     private readonly userRepository: UserRepository,
-    private readonly pusherService: PusherService
+    private readonly pusherService: PusherService,
   ) {}
 
   public async handle(payload) {
-    const chat = await this.repository.findChatByUUID(payload.chat);
-    if(!chat){
-      throw new ChatNotFound();
-    }
-    payload.sender = await this.userRepository.findUserByUUID(payload.sender);
-    payload.chat=chat;
-    
-    const sendMessage = await this.messageRepository.saveMessage(payload)
-    const channelName = `${channels.REAL_TIME_CHAT_CHANNEL}`;
-    const eventName = `${events.REAL_TIME_MESSAGE_SENT_EVENT}-${chat?.uuid}`;
-    const action = pusherAction.MESSAGE_SENT
-    await this.pusherService.trigger(channelName, eventName, { sendMessage,action });
-    return sendMessage;
+try{
+  const chat = await this.repository.findChatByUUID(payload.chat);
+  if(!chat){
+    throw new ChatNotFound();
+  }
+  payload.sender = await this.userRepository.findUserByUUID(payload.sender);
+  payload.chat=chat;
+  
+  const sendMessage = await this.messageRepository.saveMessage(payload)
+  const channelName = `${channels.REAL_TIME_CHAT_CHANNEL}`;
+  const eventName = `${events.REAL_TIME_MESSAGE_SENT_EVENT}-${chat?.uuid}`;
+  const action = pusherAction.MESSAGE_SENT
+  await this.pusherService.trigger(channelName, eventName, { sendMessage,action });
+  return sendMessage;
+}
+catch(err){
+  console.error("eerrrrrrrrrrrr===========",err);
+}
   }
 
 }
--- src/infrastructure/common/constants.ts
+++ src/infrastructure/common/constants.ts
@@ -1,15 +1,15 @@
-const channels = {
+export const channels = {
     REAL_TIME_CHAT_CHANNEL: 'real-time-chat-channel'
   }
   
-  const events = {
+  export const events = {
     REAL_TIME_MESSAGE_SENT_EVENT: 'real-time-message-sent-event',
     REAL_TIME_MESSAGE_UPDATED_EVENT : 'real-time-message-updated-event',
     REAL_TIME_INBOX_EVENT : 'real-time-inbox-event',
     REAL_TIME_MESSAGE_DELETED_EVENT : 'real-time-message-deleted-event'
   }
   
-  const pusherAction = {
+  export const pusherAction = {
     MESSAGE_SENT: 'message-sent',
     MESSAGE_UPDATE: 'message-update',
     DELETE_MESSAGE: "delete-message",
